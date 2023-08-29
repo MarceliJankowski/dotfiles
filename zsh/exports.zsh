195da4f451195789410c2a3f383e1ba2e66a37df
Marceli Jankowski <marcelijankowski123@gmail.com>
2023-08-29 22:54:04 +0200

    Add zsh config

---
+++ zsh/exports.zsh
@@ -0,0 +1,20 @@
+##################################################
+#                    EXPORTS                     #
+##################################################
+
+export readonly EDITOR="nvim"
+export readonly VISUAL="nvim"
+export readonly DOTFILES=${HOME}/dotfiles
+export readonly NOTES=${HOME}/notes
+export readonly PRETTIERD_DEFAULT_CONFIG="${DOTFILES}/prettier/prettierrc.yaml"
+
+# variables used in my shell scripts
+export readonly SHELL_SCRIPTS_PATH=${HOME}/shellScripts/scripts
+export readonly REMOTE_GIT_URL="https://github.com"
+export readonly REMOTE_GIT_USERNAME="MarceliJankowski"
+export readonly BROWSER="firefox"
+
+readonly CARGO_PATH=~/.cargo/bin # path to cargo binaries
+
+# NOTE paths from PATH variable are NOT searched recursively
+export PATH="${CARGO_PATH}:${SHELL_SCRIPTS_PATH}:${PATH}"
