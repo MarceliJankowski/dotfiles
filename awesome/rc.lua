de62b4b494680e0ff36b3fc21ae58e548285a354
Marceli Jankowski <marcelijankowski123@gmail.com>
2023-09-06 19:35:54 +0200

    Update awesome WM keymaps

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -354,10 +354,10 @@ globalkeys = gears.table.join(
 	awful.key({ modkey, "Shift" }, "k", function()
 		awful.client.swap.byidx(-1)
 	end, { description = "swap with previous client by index", group = "client" }),
-	awful.key({ modkey, "Control" }, "j", function()
+	awful.key({ modkey, "Control" }, "k", function()
 		awful.screen.focus_relative(1)
 	end, { description = "focus the next screen", group = "screen" }),
-	awful.key({ modkey, "Control" }, "k", function()
+	awful.key({ modkey, "Control" }, "j", function()
 		awful.screen.focus_relative(-1)
 	end, { description = "focus the previous screen", group = "screen" }),
 	awful.key({ modkey }, "u", awful.client.urgent.jumpto, { description = "jump to urgent client", group = "client" }),
@@ -451,6 +451,9 @@ clientkeys = gears.table.join(
 	awful.key({ modkey }, "o", function(c)
 		c:move_to_screen()
 	end, { description = "move to screen", group = "client" }),
+	awful.key({ modkey, "Shift" }, "o", function(c)
+		c:move_to_screen(c.screen.index - 1)
+	end, { description = "move to previous screen", group = "client" }),
 	awful.key({ modkey }, "t", function(c)
 		c.ontop = not c.ontop
 	end, { description = "toggle keep on top", group = "client" }),
