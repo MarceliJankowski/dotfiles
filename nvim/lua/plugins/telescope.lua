1a702c5f0853b8203f9b1cc485dc91fcc263b557
Marceli Jankowski <marcelijankowski123@gmail.com>
2024-02-16 00:35:52 +0100

    nvim: update dimensions in telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,43 +1,49 @@
-return
-	{
-		"nvim-telescope/telescope.nvim", -- fuzzy finder
-		tag = "0.1.2",
-		dependencies = {
-			"nvim-lua/plenary.nvim", -- collection of neovim lua functions
-			"nvim-telescope/telescope-media-files.nvim", -- extension for viewing media files
-		},
+return {
+	"nvim-telescope/telescope.nvim", -- fuzzy finder
+	tag = "0.1.5",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- collection of nvim lua functions
+		"nvim-telescope/telescope-media-files.nvim", -- extension for viewing media files
+	},
+	config = function()
+		local telescope = require("telescope")
 
-		config = function()
-			local telescope = require("telescope")
+		-- load media-files extension
+		telescope.load_extension("media_files")
 
-			-- load media-files extension
-			telescope.load_extension("media_files")
-
-			telescope.setup({
-				extensions = {
-					media_files = {
-						-- filetypes whitelist
-						-- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-						filetypes = { "png", "webp", "jpg", "jpeg" },
-						find_cmd = "rg", -- defaults to `fd` (ripgrep is faster)
-					},
+		telescope.setup({
+			defaults = {
+				layout_strategy = "horizontal",
+				layout_config = {
+					width = 0.92,
+					height = 0.92,
+					preview_width = 55,
 				},
-			})
+			},
+			extensions = {
+				media_files = {
+					-- filetypes whitelist
+					-- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+					filetypes = { "png", "webp", "jpg", "jpeg" },
+					find_cmd = "rg", -- defaults to `fd` (ripgrep is faster)
+				},
+			},
+		})
 
-			-- KEYMAPS
-			local telescope_builtin = require("telescope.builtin")
+		-- KEYMAPS
+		local telescope_builtin = require("telescope.builtin")
 
-			vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files)
-			vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep)
-			vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers)
-			vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags)
-			vim.keymap.set("n", "<leader>fm", telescope.extensions.media_files.media_files) -- media-files extension
+		vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files)
+		vim.keymap.set("n", "<leader>fg", telescope_builtin.live_grep)
+		vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers)
+		vim.keymap.set("n", "<leader>fh", telescope_builtin.help_tags)
+		vim.keymap.set("n", "<leader>fm", telescope.extensions.media_files.media_files)
 
-			-- show hidden files, hide '.git' directory and blobs/trees from '.gitignore'
-			vim.keymap.set(
-				"n",
-				"<leader>.",
-				"<Cmd>lua require('telescope.builtin').find_files({ find_command = { 'rg', '--files', '--hidden', '-g', '!.git' } })<CR>"
-			)
-		end,
-	}
+		-- show hidden files (except for '.git' directory), and hide blobs/trees from '.gitignore'
+		vim.keymap.set(
+			"n",
+			"<leader>.",
+			"<Cmd>lua require('telescope.builtin').find_files({ find_command = { 'rg', '--files', '--hidden', '-g', '!.git' } })<CR>"
+		)
+	end,
+}
