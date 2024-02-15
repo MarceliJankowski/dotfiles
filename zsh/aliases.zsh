9d0f6885b5d965bc1101ae5be7a2a6d3219ef5b7
Marceli Jankowski <marcelijankowski123@gmail.com>
2024-02-16 00:40:47 +0100

    zsh: add dotfiles alias and cleanse config of small inconsistencies

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,53 +3,31 @@
 ##################################################
 
 # use extended RegExp support by default
-alias sed="sed -E"
-alias grep="grep -E"
+alias sed='sed -E'
+alias grep='grep -E'
 
 # single-column colored ls
-alias ls="ls -1 --color=tty"
-alias sl="ls -1 --color=tty"
+alias ls='ls -1 --color=tty'
+alias sl='ls -1 --color=tty'
 
 # make these interactive (ask before overwriting)
-alias mv="mv -i"
-alias cp="cp -i"
+alias mv='mv -i'
+alias cp='cp -i'
 
 # curl doesn't postfix response with '\n' (newline) character by default, this causes ZSH to display it with '%' sign at the end
 # I'm "fixing" this annoying behaviour with the '-w' (write-out) flag
-alias curl="curl -w '\n'"
+alias curl='curl -w '\n''
 
 # nvim
-alias v="nvim"
-alias vi="nvim"
-alias vim="nvim"
-alias nv="nvim"
+alias v='nvim'
+alias vi='nvim'
+alias vim='nvim'
 
-# clear (yeah...)
-alias c="clear"
-alias lcs="clear"
-alias cleare ="clear"
-alias clea="clear"
-alias cear="clear"
-alias lcear="clear"
-alias clera="clear"
-alias celar="clear"
-alias cler="clear"
-alias claer="clear"
-alias clearc="clear"
-alias cleawr="clear"
-alias caler="clear"
-alias calar="clear"
-alias cclear="clear"
-alias rclear="clear"
-alias rlear="clear"
-alias rclear="clear"
-alias rcle="clear"
-alias rcler="clear"
-alias cls="clear"
-alias csl="clear"
+# get rid of GDB startup text
+alias gdb='gdb --quiet'
 
 # other
-alias gdb="gdb --quiet" # get rid of GDB startup text
-alias notes="nvim $NOTES"
-alias docker-compse="docker-compose"
-alias t="tmux"
+alias dotfiles='$EDITOR $DOTFILES'
+alias notes='$EDITOR $NOTES'
+alias docker-compse='docker-compose'
+alias t='tmux'
