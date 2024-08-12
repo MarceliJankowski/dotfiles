c1c53743276b4c2ff72137cbca04a355f5f40e04
Marceli Jankowski <marcelijankowski123@gmail.com>
2024-08-12 22:10:21 +0200

    nvim: replace lualine with staline
    Replace lualine with staline due to lualine flickering issue.
    It would sporadically flicker when interacting with Telescope or Netrw.

---
+++ nvim/lua/plugins/staline.lua
@@ -0,0 +1,51 @@
+return {
+  "tamton-aquib/staline.nvim",
+  opts = {
+    mode_icons = {
+      n = "NORMAL",
+      i = "INSERT",
+      v = "VISUAL",
+      V = "V-LINE",
+      ["\22"] = "V-BLOCK",
+      s = "SELECT",
+      S = "S-LINE",
+      ["\19"] = "S-BLOCK",
+      c = "COMMAND",
+      R = "REPLACE",
+      t = "TERMINAL",
+    },
+    sections = {
+      left = {
+        "-mode",
+        "left_sep",
+        "file_name",
+      },
+      mid = {},
+      right = {
+        "branch",
+        "right_sep",
+        "-cwd",
+      },
+    },
+    defaults = {
+      bg = "#2e3c43",
+      true_colors = true,
+      branch_symbol = " ",
+      line_column = "%l:%c",
+      left_separator = "",
+      right_separator = "",
+    },
+    mode_colors = {
+      n = "#83acff",
+      i = "#c4e98e",
+      v = "#c893eb",
+      V = "#c893eb",
+      ["\22"] = "#c893eb",
+      s = "#c893eb",
+      S = "#c893eb",
+      ["\19"] = "#c893eb",
+      R = "#f07279",
+      c = "#ffcb6b",
+    },
+  },
+}
