77184743a3f562c7ec3748bb1b1fe0466f48b70a
Marceli Jankowski <marcelijankowski123@gmail.com>
2025-04-09 10:24:08 +0200

    nvim: update lspconfig.lua diagnostic signs config to remove deprecated sign_define invocation

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -155,13 +155,6 @@ return {
     --                   VISUALS                    --
     --------------------------------------------------
 
-    -- change diagnostic symbols in the sign column (gutter)
-    local signs = { Error = "", Warn = "", Hint = "", Info = "" }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-    end
-
     -- customize how diagnostics are displayed
     vim.diagnostic.config({
       virtual_text = false,
@@ -169,6 +162,14 @@ return {
       underline = true,
       update_in_insert = false,
       severity_sort = true,
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "",
+          [vim.diagnostic.severity.WARN] = "",
+          [vim.diagnostic.severity.HINT] = "",
+          [vim.diagnostic.severity.INFO] = "",
+        },
+      },
     })
   end,
 }
