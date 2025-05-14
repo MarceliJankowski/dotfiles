9d835c673ceec602ea01183dca8b04b5aa41ecbd
Marceli Jankowski <marcelijankowski123@gmail.com>
2025-05-14 12:23:24 +0200

    nvim: update nvim-tool-installer config to include language servers

--- nvim/lua/plugins/mason/mason-tool-installer.lua
+++ nvim/lua/plugins/mason/mason-tool-installer.lua
@@ -1,16 +1,35 @@
 return {
-  "WhoIsSethDaniel/mason-tool-installer.nvim", -- installs (through mason) specified third party tools automatically
+  "WhoIsSethDaniel/mason-tool-installer.nvim", -- automatic third party tooling installation (through mason)
   dependencies = { "williamboman/mason.nvim" },
   opts = {
     ensure_installed = {
-      -- "clang-format", -- use binary from PATH to ensure formatting consistency across nvim and other tooling
+      -- language servers
+      "clangd",
+      "css-lsp",
+      "cssmodules-language-server",
+      "dockerfile-language-server",
+      "emmet-ls",
+      "eslint-lsp",
+      "html-lsp",
+      "json-lsp",
+      "lemminx",
+      "lua-language-server",
+      "marksman",
+      "phpactor",
+      "pyright",
+      "sqlls",
+      "typescript-language-server",
+      "vim-language-server",
+      "yaml-language-server",
+
+      -- miscellaneous
       "stylua",
       "prettierd",
       "shfmt",
-      "php-cs-fixer",
       "latexindent",
-      "phpstan",
       "ruff",
+      "phpstan",
+      -- "clang-format", -- use binary from PATH to ensure formatting consistency across nvim and other tooling
     },
   },
 }
