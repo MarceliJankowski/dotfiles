75f0ba2f62c7d443dd8b9b4454831e411b50ab1f
Marceli Jankowski <marcelijankowski123@gmail.com>
2026-04-06 12:41:50 +0200

    nvim: change color of cursor and its underlying line

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -9,6 +9,15 @@ return {
       colors.bg_dark = "#000000"
       colors.bg_float = "#000000"
     end,
+    on_highlights = function(hl)
+      hl.CursorLine = {
+        bg = "#0d0d0d",
+      }
+      hl.Cursor = {
+        fg = "#000000",
+        bg = "#ffcb6b",
+      }
+    end,
   },
   config = function(_, opts)
     require("tokyonight").setup(opts)
