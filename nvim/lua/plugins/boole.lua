b85ba2c216f5a8405a0c71d4343243f7ddc472dc
Marceli Jankowski <marcelijankowski123@gmail.com>
2026-04-16 16:50:50 +0200

    nvim: remove boole plugin

--- nvim/lua/plugins/boole.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nat-418/boole.nvim", -- extends default increment and decrement with <C-x> and <C-a> functionality
-  opts = {
-    mappings = {
-      increment = "<C-a>",
-      decrement = "<C-x>",
-    },
-  },
-}
