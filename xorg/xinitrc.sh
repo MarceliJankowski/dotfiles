4392987faa37ea76d33b9fade3ea53cbe184ecb8
Marceli Jankowski <marcelijankowski123@gmail.com>
2026-04-19 14:31:11 +0200

    xorg: simplify xinitrc.sh

--- xorg/xinitrc.sh
+++ xorg/xinitrc.sh
@@ -2,10 +2,10 @@
 
 # execute scripts in /etc/X11/xinit/xinitrc.d directory
 if [ -d /etc/X11/xinit/xinitrc.d ]; then
-	for f in /etc/X11/xinit/xinitrc.d/*.sh; do
-		[ -x "$f" ] && . "$f"
-	done
-	unset f
+  for f in /etc/X11/xinit/xinitrc.d/*.sh; do
+    [ -x "$f" ] && . "$f"
+  done
+  unset f
 fi
 
 # start compositor
@@ -14,16 +14,5 @@ picom &
 # bind capslock to escape
 setxkbmap -option "caps:escape"
 
-# set 'awesome' as the default WM in case no arguments were passed to this script
-session=${1:-awesome}
-
-case $session in
-awesome) exec awesome ;;
-bspwm) exec bspwm ;;
-dwm) exec dwm ;;
-i3 | i3wm) exec i3 ;;
-xmonad) exec xmonad ;;
-
-# unrecognized session, try running it as command
-*) exec $1 ;;
-esac
+# start awesome WM
+exec awesome
